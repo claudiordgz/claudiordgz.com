0bb374903595acbe1849b0ff6303502d74c1f2f1
Claudio Rodriguez <claudio.rdgz@gmail.com>
2018-09-16 17:30:15 -0400

    test: check for null and not null strings

--- ContentManager/ContentManager.Test/FileManagerBlogShould.cs
+++ ContentManager/ContentManager.Test/FileManagerBlogShould.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Text;
 using Xunit;
@@ -21,13 +22,11 @@ namespace ContentManager.Test
         }
 
         [Fact]
-        public void ReturnsExpectedFrontMatter()
+        public void ParsesTitleSlugAndDraftFlag ()
         {
             string mockPost = @"---
 title: Docker Installation Notes
 slug: docker-installation-notes
-date_published: 1970-01-01T00:00:00.000Z
-date_updated:   2014-12-05T01:10:29.904Z
 draft: true
 ---
 <my-component>Some-data</my-component>
@@ -35,7 +34,31 @@ draft: true
             var frontMatter = FileManager.GetFrontMatter(mockPost);
             Assert.Equal("Docker Installation Notes", frontMatter.Title);
             Assert.Equal("docker-installation-notes", frontMatter.Slug);
+            Assert.Null(frontMatter.Published);
+            Assert.Null(frontMatter.Updated);
             Assert.True(frontMatter.IsDraft);
+        }
+
+        [Fact]
+        public void ParsesDateTimes ()
+        {
+            var builder = new StringBuilder();
+            var publishedDate = "1970-01-01T00:00:00.0000000Z";
+            var updatedDate = "2014-12-05T01:10:29.9040000Z";
+            builder.AppendFormat(@"---
+title: Some Title
+slug: some-slug
+date_published: {0}
+date_updated: {1}
+---
+<my-component>Some-data</my-component>
+", publishedDate, updatedDate);
+            var mockPost = builder.ToString();
+            var frontMatter = FileManager.GetFrontMatter(mockPost);
+            string pbDate = frontMatter.Published.Value.ToString("o");
+            string upDate = frontMatter.Updated.Value.ToString("o");
+            Assert.Equal(publishedDate, pbDate);
+            Assert.Equal(updatedDate, upDate);
         }
     }
 }
