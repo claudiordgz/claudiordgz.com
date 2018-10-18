2149b78653c1a31e5bf92e60a3f0d9e07d9c0c7b
claudiordgz <claudio.rdgz@gmail.com>
2018-10-18 04:50:13 -0400

    test: github properties debugging

--- ContentManager/ContentManager.Test/GithubUtilsShould.cs
+++ ContentManager/ContentManager.Test/GithubUtilsShould.cs
@@ -1,21 +1,39 @@
 ﻿using Xunit;
 using FluentAssertions;
+using Xunit.Abstractions;
+using ContentManager.Util;
 
 namespace ContentManager.Test
 {
     public class GithubUtilsShould
     {
+        private readonly ITestOutputHelper output;
+
+        public GithubUtilsShould(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         [Fact]
         public void GetsRepositoryProperties()
         {
+
             string srcPath = GetSrc.SrcPath();
-            Util.RepositorySettings props = Util.Git.GetCurrentGithubProperties(srcPath);
-            props.User.Should().NotBeEmpty("User needs to be defined");
-            props.Branch.Should().NotBeEmpty("Branch needs to be defined");
-            props.Repository.Should().NotBeEmpty("Repository needs to be defined");
-            props.User.Should().NotBeNullOrWhiteSpace("User needs to be defined");
-            props.Branch.Should().NotBeNullOrWhiteSpace("Branch needs to be defined");
-            props.Repository.Should().NotBeNullOrWhiteSpace("Repository needs to be defined");
+            try
+            {
+                RepositorySettings props = Git.GetCurrentGithubProperties(srcPath);
+                props.User.Should().NotBeEmpty("User needs to be defined");
+                props.Branch.Should().NotBeEmpty("Branch needs to be defined");
+                props.Repository.Should().NotBeEmpty("Repository needs to be defined");
+                props.User.Should().NotBeNullOrWhiteSpace("User needs to be defined");
+                props.Branch.Should().NotBeNullOrWhiteSpace("Branch needs to be defined");
+                props.Repository.Should().NotBeNullOrWhiteSpace("Repository needs to be defined");
+            } catch (GithubUtilFatalError e)
+            {
+                output.WriteLine("Error on github properties");
+                output.WriteLine(e.Message);
+                e.Should().BeNull();
+            }
         }
     }
 }
